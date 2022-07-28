f2c529cb47150c6ceac8d9a77d8a501d9ea98431
samv <mariscal@gmail.com>
2022-07-28 10:22:02 -0500

    todo layour

--- src/app/layout/contenido/contenido/contenido.component.ts
+++ src/app/layout/contenido/contenido/contenido.component.ts
@@ -1,37 +1,16 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { GeneralesService } from 'src/app/shared/services/generales.service';
 
 @Component({
   selector: 'app-contenido',
   templateUrl: './contenido.component.html',
-  styleUrls: ['./contenido.component.scss'],
-  animations: [
-    trigger("abriendomenu", [
-      state("abrir", style({
-        display: 'block',
-      })),
-      state("cerrar", style({
-        width: '0px',
-        opacity: 0.0
-      })),
-      transition("abrir => cerrar", [
-        animate('0.2s')
-      ]),
-      transition("cerrar => abrir", [
-        animate('0.2s')])]
-    )]
+  styleUrls: ['./contenido.component.scss']
 })
 export class ContenidoComponent implements OnInit {
   public mostrar:boolean = true;
 
-  constructor(private generalesPrd:GeneralesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.generalesPrd.serviciomenu().subscribe(datos =>{
-      console.log("entra");
-      this.mostrar = !this.mostrar;
-    });
   }
 
 }
--- src/app/layout/contenido/menu/menu.component.ts
+++ src/app/layout/contenido/menu/menu.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralesService } from 'src/app/shared/services/generales.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger("abriendomenu", [
+      state("abrir", style({
+        display: 'block',
+      })),
+      state("cerrar", style({
+        width: '0px',
+        opacity: 0.0
+      })),
+      transition("abrir => cerrar", [
+        animate('0.2s')
+      ]),
+      transition("cerrar => abrir", [
+        animate('0.2s')])]
+    )]
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public mostrar:boolean = true;
+
+  constructor(private generalesPrd:GeneralesService) { }
 
   ngOnInit(): void {
+    this.generalesPrd.serviciomenu().subscribe(datos =>{
+      console.log("entra");
+      this.mostrar = !this.mostrar;
+    });
   }
 
 }
