3f591ce4945ea148b01b23e378b9a68154bfc430
samv <mariscal@gmail.com>
2022-10-21 07:54:51 -0500

    servicios final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,13 @@ export const environment = {
   urlAdmin: "http://ec2-54-170-164-250.eu-west-1.compute.amazonaws.com:3000",
   configuracionCognito:{
     Auth:  {
-      userPoolId: 'us-east-2_pptCj2gqV',
-      userPoolWebClientId: '1452opnjll0ldmocs201b1oimu',
+      userPoolId: 'us-east-2_m8LZLLjoI',
+      userPoolWebClientId: '2c26av0fl2vt8v8k823o8jsphg',
+
     },
+    jscognito:{
+      UserPoolId: 'us-east-2_m8LZLLjoI', 
+      ClientId: '2c26av0fl2vt8v8k823o8jsphg'
+    }
   }
 };
